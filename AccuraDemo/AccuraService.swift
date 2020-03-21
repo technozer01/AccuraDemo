dbb44ad303d6c883070e8cff91ceb36a9f22a80a
kuldeep <kuldeep@technozer.com>
2020-03-21 12:01:21 +0530

    AccuraDemo Finish

---
+++ AccuraDemo/AccuraService.swift
@@ -0,0 +1,18 @@
+//
+//  AccuraService.swift
+//  AccuraDemo
+//
+//  Created by Technozer on 21/03/20.
+//  Copyright © 2020 Technozer. All rights reserved.
+//
+
+import Foundation
+
+public class AccuraService{
+    
+    private init() {}
+    
+    public static func doSamthing() -> String{
+        return "Success Demo"
+    }
+}
