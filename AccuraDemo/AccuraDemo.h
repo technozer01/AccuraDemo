b2ee16f7ae90505cafbc61bf0a0265c0b8839dd2
kuldeep <kuldeep@technozer.com>
2020-03-21 11:52:47 +0530

    Initial Commit

---
+++ AccuraDemo/AccuraDemo.h
@@ -0,0 +1,19 @@
+//
+//  AccuraDemo.h
+//  AccuraDemo
+//
+//  Created by Technozer on 21/03/20.
+//  Copyright © 2020 Technozer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AccuraDemo.
+FOUNDATION_EXPORT double AccuraDemoVersionNumber;
+
+//! Project version string for AccuraDemo.
+FOUNDATION_EXPORT const unsigned char AccuraDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccuraDemo/PublicHeader.h>
+
+
